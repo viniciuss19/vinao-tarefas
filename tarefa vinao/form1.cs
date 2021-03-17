97a0b264968cb803809233daff921ad43a091e55
viniciuss19 <79714215+viniciuss19@users.noreply.github.com>
2021-03-17 20:45:59 -0300

    Add files via upload
    commit ai graças a deus

---
+++ tarefa vinao/form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tarefa_vinao
+{
+    public partial class exercíço : Form
+    {
+        public exercíço()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
